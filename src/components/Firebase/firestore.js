43c82237dc82f98a74a8d79ddb83329b1228ac72
Piotr Szocik <piotrek.szocik@gmail.com>
2021-11-12 16:47:52 +0100

    minor refactor

--- src/components/Firebase/firestore.js
+++ src/components/Firebase/firestore.js
@@ -5,20 +5,25 @@ import { doc, setDoc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
 const db = getFirestore(FirebaseApp);
 
 const createDocument = (email) => {
-  return setDoc(doc(db, "highscores", email), {
+  const highscoresRef = doc(db, "highscores", email);
+  setDoc(highscoresRef, {
     scores: [],
-  });
+  })
+    .then((data) => console.log("Document created successfully."))
+    .catch((err) => console.log(err));
 };
 
-const updateScores = async (email, score) => {
+const updateScores = (email, score) => {
   const userScoresRef = doc(db, "highscores", email);
   const today = new Date();
   const date =
     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   const time = today.getHours() + ":" + today.getMinutes();
-  await updateDoc(userScoresRef, {
+  updateDoc(userScoresRef, {
     scores: arrayUnion(`${date} ${time} Points: ${score}`),
-  });
+  })
+    .then((data) => console.log("Highscores updated successfully!"))
+    .catch((err) => console.log(err));
 };
 
 const getScores = async (email) => {
